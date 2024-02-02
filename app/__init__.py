4d29ba146a0f179841562299a2996915cdb3afe2
Talamarla Premanath <premanath@syoft.com>
2024-02-02 22:33:30 +0530

    rds database added

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import awsgi
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
@@ -17,3 +18,7 @@ api.add_resource(UserResource, "/users", "/users/<int:user_id>")
 
 with app.app_context():
     db.create_all()
+
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context, base64_content_types={"image/png"})
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///prema2.db'
 
     # local postgresql
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/icompass_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://iCompass_db:Prema143@database-1.cfsqyk4ui4co.ap-south-1.rds.amazonaws.com/initial_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
