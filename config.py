1030a8750520394e5b67ec7ce61c86a474dba4e0
Talamarla Premanath <premanath@syoft.com>
2024-02-02 20:20:18 +0530

    local postgresql added

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///prema2.db'
+    # sqlite3
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///prema2.db'
+
+    # local postgresql
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/icompass_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
